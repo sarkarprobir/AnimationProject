a0343df766176ecf43d58aadd2fa13f97f1d9735
argha <arghadeep2000m@gmail.com>
2025-05-27 11:36:27 +0530

    create shapes popup, change button border radius and slides name and button border

--- AnimationProject/wwwroot/js/animations.js
+++ AnimationProject/wwwroot/js/animations.js
@@ -3939,3 +3939,18 @@ function handleThumbClick(clickedElement) {
     items.forEach(el => el.classList.remove('active_border'));
     clickedElement.classList.add('active_border');
 }
+
+function toggleShapePopup() {
+    const popup = document.getElementById('shapePopup');
+    popup.style.display = (popup.style.display === 'block') ? 'none' : 'block';
+}
+
+// Optional: click outside to close
+document.addEventListener('click', function (event) {
+    const popup = document.getElementById('shapePopup');
+    const button = document.getElementById('shapeToggleBtn');
+
+    if (!popup.contains(event.target) && !button.contains(event.target)) {
+        popup.style.display = 'none';
+    }
+});
