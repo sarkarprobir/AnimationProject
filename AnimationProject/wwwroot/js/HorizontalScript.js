d3b39101bb48c704366fc06fa6439b3ad1766fde
argha <arghadeep2000m@gmail.com>
2025-07-19 12:23:21 +0530

    add zoom functionality

--- AnimationProject/wwwroot/js/HorizontalScript.js
+++ AnimationProject/wwwroot/js/HorizontalScript.js
@@ -462,3 +462,35 @@ document.addEventListener('click', function (event) {
         popup.style.display = 'none';
     }
 });
+
+//zoom function
+
+//let scale = 1;
+//const scaleStep = 0.1;
+//const maxScale = 3;
+//const minScale = 0.5;
+
+//const canvas = document.getElementById("myHorizCanvas");
+//const scaleText = document.getElementById("scaleValue");
+
+//function applyScale() {
+//    canvas.style.transform = `scale(${scale})`;
+//    scaleText.textContent = `Scale: ${scale.toFixed(1)}`;
+//}
+
+//function zoomIn() {
+//    if (scale < maxScale) {
+//        scale += scaleStep;
+//        applyScale();
+//    }
+//}
+
+//function zoomOut() {
+//    if (scale > minScale) {
+//        scale -= scaleStep;
+//        applyScale();
+//    }
+//}
+
+
+//zoom function end
