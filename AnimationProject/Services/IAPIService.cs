77e09649701eb15e60e7cbbdc431ad10e6ac7f77
sunil <sm@hellocph.com>
2025-10-10 12:13:07 +0530

    Add API Service dt. 09.10.2025

---
+++ AnimationProject/Services/IAPIService.cs
@@ -0,0 +1,8 @@
+﻿namespace AnimationProject.Services
+{
+    public interface IAPIService
+    {
+        Task<T?> GetAsync<T>(string endpoint, Dictionary<string, string?>? queryParams = null);
+        Task<TResponse?> PostAsync<TRequest, TResponse>(string endpoint, TRequest data);
+    }
+}
